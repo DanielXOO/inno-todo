e074f6eba33c832fe35f69374a97309429966ffc
DanielXOO <dankulinkovich@gmail.com>
2023-04-04 08:46:18 +0300

    remove unused css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginForm from './containers/LoginForm';
 import RegisterForm from './containers/RegisterForm';
