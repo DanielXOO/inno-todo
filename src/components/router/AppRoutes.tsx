ac93f0b6b4103b132ffaea7493615709847c7a30
DanielXOO <dankulinkovich@gmail.com>
2023-06-09 16:11:29 +0300

    finish ProtectedRoute.tsx

---
+++ src/components/router/AppRoutes.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginForm from '../../containers/LoginForm';
+import ProtectedRoute from './ProtectedRoute';
+import RegisterForm from '../../containers/RegisterForm';
+import { useAuth } from '../auth/AuthProvider';
+import Loader from '../ui/Loader';
+
+const AppRoutes: React.FC = () => {
+  const { isLoading } = useAuth();
+
+  return (
+    <Loader isLoading={isLoading}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<LoginForm />} />
+          <Route
+            path="/signup"
+            element={
+              <ProtectedRoute redirectPath="/signin">
+                <RegisterForm />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Loader>
+  );
+};
+
+export default AppRoutes;
